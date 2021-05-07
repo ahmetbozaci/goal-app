411f1e7d80c5a6af8837a9c6920352923ed565d7
Ahmet Bozaci <ahmt9417@gmail.com>
2021-05-07 15:15:09 +0300

    Create session controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+
+  def create
+    user = User.find_by(id: session[:user_id])
+    if user.present?
+      session[:user_id] = user.id
+      flash[:notice] = 'Logged in successfully'
+    else
+      flash[:alert] = 'Username is not correct'
+      render :new
+    end
+
+  end
+  
+  def destroy
+    session[:user_id] = nil
+    flash[:notice] = 'Logged out'
+    redirect_to root_path
+  end
+  
+end
