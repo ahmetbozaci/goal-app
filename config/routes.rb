3f6197517a9c75de06a2be94df26cdc713ddb5d8
Ahmet Bozaci <ahmt9417@gmail.com>
2021-05-07 15:19:11 +0300

    Edit route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   root 'home#index'
   resources :users
+
+  get 'sign_up', to: 'users#new'
+  post 'sign_up', to: 'users#create'
+
+  get 'sign_in', to: 'sessions#new'
+  post 'sign_in', to: 'sessions#create'
+
+  delete 'logout', to: 'sessions#destroy'
 end
