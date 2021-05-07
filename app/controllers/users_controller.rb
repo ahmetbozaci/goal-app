0693271e22d47b8402fbea7c21c7f5c574ff66f9
Ahmet Bozaci <ahmt9417@gmail.com>
2021-05-07 15:11:47 +0300

    Create method in user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,26 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      session[:user_id] = @user.id
+      flash[:notice] = "#{@user.name} is created successfully"
+      redirect_to root_path
+    else
+      render :new
+  end
+
+  def show
+    @user = User.find_by(:id session[:user_id] if session[:user_id])
+  end
+
+  private
+
+  def user_params
+    param.require(:user).permit(:name)
+  end
 end
