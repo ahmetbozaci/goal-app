3c9dd920d69afa38b7e93ea1736215ec8ed374b1
Ahmet Bozaci <ahmt9417@gmail.com>
2021-05-09 15:32:39 +0300

    Edit notice message

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,8 +7,8 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      #flash[:notice] = "#{@user.name} is created successfully"
-      redirect_to root_path, notice: "Succesfully created"
+      flash[:notice] = "#{@user.name} is created successfully"
+      redirect_to root_path
     else
       render :new
     end
