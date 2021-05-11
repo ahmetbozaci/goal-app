98ec3024f5adfad957434bb374e0b414dc7fa65b
Ahmet Bozaci <ahmt9417@gmail.com>
2021-05-12 01:37:23 +0300

    Add test for goal model

--- spec/model/goal_spec.rb
+++ spec/model/goal_spec.rb
@@ -1,1 +1,32 @@
 require 'rails_helper'
+
+RSpec.describe Goal, type: 'model' do
+  before :each do
+    @user = User.create(name: 'TestName')
+    @goal = @user.goals.new(name: 'New Test', amount: 10)
+  end
+
+  describe 'associations' do
+    it 'belongs to user' do
+      expect(@goal).to respond_to(:user)
+    end
+
+    # it 'has many goals' do
+    #   expect(@group).to respond_to(:goals)
+    # end
+  end
+
+  it 'name should not be empty' do
+    test_goal = Goal.new
+    test_goal.name = nil
+    expect(test_goal).to_not be_valid
+  end
+
+  it 'should not be empty' do
+    test_goal = Goal.new
+    test_goal.name = 'New Goal'
+    test_goal.amount = 10
+    test_goal.user_id = 1
+    expect(test_goal).to be_valid
+  end
+end
