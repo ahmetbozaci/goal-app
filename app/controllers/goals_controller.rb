4517a94052a3a89fc925a46c5da8d714815d7007
Ahmet Bozaci <ahmt9417@gmail.com>
2021-05-20 16:21:51 +0300

    Style create goal page

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -29,7 +29,7 @@ class GoalsController < ApplicationController
 
     respond_to do |format|
       if @goal.save
-        format.html { redirect_to @goal, notice: 'Goal was successfully created.' }
+        format.html { redirect_to goals_path, notice: 'Goal was successfully created.' }
         format.json { render :show, status: :created, location: @goal }
       else
         format.html { render :new, status: :unprocessable_entity }
